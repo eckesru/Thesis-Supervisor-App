d801ceb3c3b520f6683b63ef57dfd540025cdd6e
eckesru <rudi.eckes@gmail.com>
2023-10-20 20:07:59 +0200

    SupervisorBoardActivity und item_supervisor implementiert

--- app/src/main/java/de/iu/iwmb02_iu_betreuer_app/presentation/activities/SupervisorBoardActivity.java
+++ app/src/main/java/de/iu/iwmb02_iu_betreuer_app/presentation/activities/SupervisorBoardActivity.java
@@ -1,8 +1,10 @@
-package de.iu.iwmb02_iu_betreuer_app;
+package de.iu.iwmb02_iu_betreuer_app.presentation.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import de.iu.iwmb02_iu_betreuer_app.R;
 
 public class SupervisorBoardActivity extends AppCompatActivity {
 

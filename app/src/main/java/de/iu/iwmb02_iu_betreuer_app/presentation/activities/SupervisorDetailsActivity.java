868745b6d2e80e0c43be898e159d9b61e90e3161
eckesru <rudi.eckes@gmail.com>
2023-10-21 12:46:25 +0200

    SupervisorDetailsActivity implementiert

---
+++ app/src/main/java/de/iu/iwmb02_iu_betreuer_app/presentation/activities/SupervisorDetailsActivity.java
@@ -0,0 +1,16 @@
+package de.iu.iwmb02_iu_betreuer_app.presentation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import de.iu.iwmb02_iu_betreuer_app.R;
+
+public class SupervisorDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supervisor_details);
+    }
+}
